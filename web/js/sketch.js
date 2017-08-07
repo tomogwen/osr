a1bce54504d506add179337a1cf3de1c54f08727
Tom Davies <tom.davies.96@hotmail.co.uk>
2017-08-07 12:54:48 +0100

    no message

--- web/js/sketch.js
+++ web/js/sketch.js
@@ -1,40 +1,50 @@
 // Make an instance of two and place it on the page.
 var elem = document.getElementById('draw-shapes');
-var params = { width: 280, height: 280 };
+var params = { width: 560, height: 560 };
 var two = new Two(params).appendTo(elem);
 
 // two has convenience methods to create shapes.
 
-var rect = two.makeRectangle(140, 140, 280, 280);
+var rect = two.makeRectangle(280, 280, 560, 560);
 
 // The object returned has many stylable properties:
 
+var mapArray = new Array();
+for(var i = 0; i < 28; i++) {
+  mapArray[i] = new Array;
+  for(var j = 0; j < 28; j++) {
+    mapArray[i][j] = 1;
+  }
+}
 
 rect.fill = 'rgb(210, 210, 210)';
 rect.opacity = 0.75;
 rect.noStroke();
-two.makeLine(140,5,   140,275);
-two.makeLine(5,  140, 275,140);
-
+two.makeLine(280,10,  280,550);
+two.makeLine(10, 280, 550,280);
 
 document.addEventListener("mousemove", function(event){
     event.preventDefault();
     if (event.which == 1) {
       var circle = two.makeCircle(event.pageX-10, event.pageY-10, 1);
+      if ( (Math.floor((event.pageY-10)/20) < 28 ) && (Math.floor((event.pageX-10)/20) < 28 ) )  {
+        mapArray[Math.floor((event.pageX-10)/20)][Math.floor((event.pageY-10)/20)] = 0;
+      }
     }
-
 });
 
 document.addEventListener("click", function(event){
     event.preventDefault();
+    console.log(mapArray);
     two.clear();
-    var rect = two.makeRectangle(140, 140, 280, 280);
+    var rect = two.makeRectangle(280, 280, 560, 560);
     rect.fill = 'rgb(210, 210, 210)';
     rect.opacity = 0.75;
     rect.noStroke();
-    two.makeLine(140,5,   140,275);
-    two.makeLine(5,  140, 275,140);
+    two.makeLine(280,10,  280,550);
+    two.makeLine(10, 280, 550,280);
 });
+
 
 // Don't forget to tell two to render everything
 // to the screen
