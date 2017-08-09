f50f1836eec4b62615256f120b580f16da0a831e
Tom Davies <tom.davies.96@hotmail.co.uk>
2017-08-09 15:27:08 +0100

    Saves incorrect data

--- web/js/sketch.js
+++ web/js/sketch.js
@@ -7,17 +7,63 @@ var http = new XMLHttpRequest();
 var url = "http://tomogwen.me:1234";
 var areButtons = 0;
 
+
 http.open("POST", url, true);
 
-function checkResponse(mapArray, bestGuess) {
+function textToLabel (text) {
+    var number;
+    switch(text) {
+      case "Positive Linear":
+        number = 0;
+        break;
+      case "Negative Linear":
+        number = 1;
+        break;
+      case "Positive Quadratic":
+        number = 2;
+        break;
+      case "Negative Quadratic":
+        number = 3;
+        break;
+      case "Sine":
+        number = 4;
+        break;
+      case "Cosine":
+        number = 5;
+        break;
+      default:
+        number = 9;
+    }
+    return number;
+}
+
+function checkResponse(mapArray, bestGuess, mapArraySaved) {
     document.getElementById("request").innerHTML = "Help me learn. Was I correct?";
-    if(areButtons == 0 ){
+
+    if (areButtons == 0) {
       var button = document.createElement("input");
       button.type = "button";
       button.value = "Yes";
       button.name = "correct";
       button.onclick = function() {
-        alert("yay");
+
+        mapFlat = flatten(mapArraySaved);
+        mapFlat.unshift(1,textToLabel(bestGuess));
+        http.open("POST", url, true);
+        http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        mapFlat = mapFlat.toString();
+        mapFlat = mapFlat.replace(/,/g , "");
+        http.send(mapFlat);
+
+        document.getElementById("buttons").removeChild(button)
+        document.getElementById("buttons").removeChild(button1)
+        areButtons = 0;
+
+        for(var i = 0; i < 28; i++) {
+          for(var j = 0; j < 28; j++) {
+            mapArraySaved[i][j] = 1;
+          }
+        }
       }
       document.getElementById("buttons").appendChild(button)
       var button1 = document.createElement("input");
@@ -25,12 +71,65 @@ function checkResponse(mapArray, bestGuess) {
       button1.value = "No";
       button1.name = "correct";
       button1.onclick = function() {
-        alert("aw");
+
+        var newList = document.createElement("select");
+        newList.name = "optionList";
+        newList.id = "optionList";
+        newList.appendChild(new Option("Positive Linear", "0"));
+        newList.appendChild(new Option("Negative Linear", "1"));
+        newList.appendChild(new Option("Positive Quadratic", "2"));
+        newList.appendChild(new Option("Negative Quadratic", "3"));
+        newList.appendChild(new Option("Sine", "4"));
+        newList.appendChild(new Option("Cosine", "5"));
+
+        document.getElementById("buttons").appendChild(newList);
+
+        var button2 = document.createElement("input");
+        button2.type = "button";
+        button2.value = "Submit";
+        button2.name = "submit";
+        button2.onclick = function() {
+
+          mapFlat = flatten(mapArraySaved);
+          mapFlat.unshift(1, document.getElementById("optionList").value);
+          http.open("POST", url, true);
+          http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+          mapFlat = mapFlat.toString();
+          mapFlat = mapFlat.replace(/,/g , "");
+          http.send(mapFlat);
+
+          document.getElementById("buttons").removeChild(button)
+          document.getElementById("buttons").removeChild(button1)
+          areButtons = 0;
+          document.getElementById("buttons").removeChild(button2)
+          document.getElementById("buttons").removeChild(newList)
+
+          for(var i = 0; i < 28; i++) {
+            for(var j = 0; j < 28; j++) {
+              mapArraySaved[i][j] = 1;
+            }
+          }
+
+        }
+        document.getElementById("buttons").appendChild(button2)
+
       }
       document.getElementById("buttons").appendChild(button)
       document.getElementById("buttons").appendChild(button1)
+      areButtons = 1;
     }
-    areButtons = 1;
+
+    for(var i = 0; i < 28; i++) {
+      for(var j = 0; j < 28; j++) {
+        mapArraySaved[i][j] = mapArray[i][j];
+      }
+    }
+    // reset map array WARNING CHANGES RESET POSITION
+    for(var i = 0; i < 28; i++) {
+      for(var j = 0; j < 28; j++) {
+        mapArray[i][j] = 1;
+      }
+    }
 }
 
 
@@ -38,13 +137,17 @@ http.onreadystatechange = function() {//Call a function when the state changes.
     if(http.readyState == 4 && http.status == 200) {
         var bestGuess = http.responseText;
         console.log(bestGuess);
-        document.getElementById("label").innerHTML = "I guessed: " + bestGuess;
-        checkResponse(mapArray, bestGuess);
+        alert(bestGuess);
 
-        for(var i = 0; i < 28; i++) {
-          for(var j = 0; j < 28; j++) {
-            mapArray[i][j] = 1;
-          }
+        if(bestGuess == "datainvalid") {
+          document.getElementById("request").innerHTML = "Invalid data sent to API, please register an issue at http://github.com/tomogwen/osr";
+        }
+        else if (bestGuess == 'datasaved') {
+          document.getElementById("request").innerHTML = "Thank you for helping me to learn";
+        }
+        else {
+          document.getElementById("label").innerHTML = "I guessed: " + bestGuess;
+          checkResponse(mapArray, bestGuess, mapArraySaved);
         }
     }
 }
@@ -56,6 +159,13 @@ for(var i = 0; i < 28; i++) {
   mapArray[i] = new Array;
   for(var j = 0; j < 28; j++) {
     mapArray[i][j] = 1;
+  }
+}
+var mapArraySaved = new Array();
+for(var i = 0; i < 28; i++) {
+  mapArraySaved[i] = new Array;
+  for(var j = 0; j < 28; j++) {
+    mapArraySaved[i][j] = mapArray[i][j];
   }
 }
 
@@ -114,13 +224,12 @@ document.addEventListener("click", function(event){
       drawAxis();
 
       mapFlat = flatten(mapArray);
-
+      mapFlat.unshift(0,0);
       http.open("POST", url, true);
       http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
       mapFlat = mapFlat.toString();
       mapFlat = mapFlat.replace(/,/g , "");
       http.send(mapFlat);
-
 
     }
 });
